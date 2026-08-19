a1b4c85a93928ec1137d7f9d6979cf359138a63e
Zsolt Bea <zsolt.bea@MacBook-Pro---Zsolt.local>
2026-08-19 17:26:02 +0100

    Add lang chain test file

---
+++ lc-test.py
@@ -0,0 +1,17 @@
+# pip install -qU langchain langchain-ollama
+from langchain.agents import create_agent
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="ollama:devstral-2",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
+)
+print(result["messages"][-1].content_blocks)
