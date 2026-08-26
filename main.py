760b87d3bdfcd475aee429ae126f93e7b3e21888
Zsolt <zsolt.bea@clearroute.io>
2026-08-26 13:20:44 +0100

    Imrove logging and remove reundant logs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from modes.scan import scan_repositories
 
-print("--- START ---")
+print("\n\n*** START ***")
 
 parser = argparse.ArgumentParser(description="This is a Repository Intelligence Agent. It can analyse multiple repositories and provide insights about them.")
 parser.add_argument("mode", choices=["scan", "analyse"], help="Mode of operation: 'scan' to scan repositories, 'analyse' to analyse scanned data.")
@@ -9,10 +9,10 @@ parser.add_argument("path", type=str, help="Path to the repository folder to ana
 
 args = parser.parse_args()
 
-print(f"Mode: {args.mode}")
+print(f"*** Mode: {args.mode} ***\n")
 
 if args.mode == "scan":
-    print(f"Scanning repositories in: {args.path}")
+    print(f"Scanning for repositories in: {args.path}\n")
     scan_repositories(args.path)
 
 
